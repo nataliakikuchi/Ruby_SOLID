e80cf974f7538c8b10ce8f97eb02e72bc891ef6f
Natalia Kikuchi <nataliakikuchi90@gmail.com>
2020-11-26 14:33:21 -0300

    Corrects test

--- OCP/tests/app/delivery_to_stdout_spec.rb
+++ OCP/tests/app/delivery_to_stdout_spec.rb
@@ -3,9 +3,8 @@ require './app/delivery_to_stdout'
 RSpec.describe DeliveryToStdout do
   context 'when receiving a text' do
     it 'delivers text to STDOUT' do
-      logger_to_stdout = double('Logger')
+      logger_to_stdout = class_spy('Logger')
       text = 'Olá, mundo!'
-      allow(logger_to_stdout).to receive(:write).with(text)
 
       subject = described_class.new(logger_to_stdout: logger_to_stdout)
       subject.deliver(text)
