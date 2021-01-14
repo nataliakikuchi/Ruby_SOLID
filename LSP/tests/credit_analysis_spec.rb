488d4daf38228c861a8e0a96574870dade080c94
Natalia Kikuchi <nataliakikuchi90@gmail.com>
2021-01-13 21:29:12 -0300

    Implements tests

---
+++ LSP/tests/credit_analysis_spec.rb
@@ -0,0 +1,15 @@
+require './app/credit_analysis'
+
+RSpec.describe CreditAnalysis do
+  context 'analysing an application' do
+    it 'calls the analyst to analyse the application' do
+      analyst = spy('Analyst')
+      application = double('Application')
+
+      subject = described_class.new([analyst], [application])
+      subject.analyse_all_applications
+
+      expect(analyst).to have_received(:analyse).with(application)
+    end
+  end
+end
