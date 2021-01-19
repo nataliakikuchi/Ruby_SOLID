d16b454e757551d1632a92a8b42ad43ce60c51c7
Natalia Kikuchi <nataliakikuchi90@gmail.com>
2021-01-19 17:14:51 -0300

    Cria arquivos para a classe automatic decision analyst

---
+++ LSP/app/automatic_decision_analyst.rb
@@ -0,0 +1,11 @@
+require_relative 'analyst'
+
+class AutomaticDecisionAnalyst < Analyst
+  def analyse(application, reject)
+    if reject
+      puts "#{name} approved #{application.customer_name}'s application!"
+    else
+      puts "#{name} rejected #{application.customer_name}'s application."
+    end
+  end
+end
---
+++ LSP/tests/automatic_decision_analyst_spec.rb
@@ -0,0 +1,27 @@
+require './app/automatic_decision_analyst'
+
+RSpec.describe AutomaticDecisionAnalyst do
+  context 'when analysing an application' do
+    context 'and the analysis is approved' do
+      it 'returns that the application is approved' do
+        application = double('Application', customer_name: 'Zé')
+        name = 'Natália'
+        result =  "#{name} approved #{application.customer_name}'s application!\n"
+
+        subject = described_class.new(name)
+
+        expect { subject.analyse(application, true) }.to output(result).to_stdout
+      end
+
+      it 'returns that the application is rejected' do
+        application = double('Application', customer_name: 'Zé')
+        name = 'Natália'
+        result =  "#{name} rejected #{application.customer_name}'s application!\n"
+
+        subject = described_class.new(name)
+
+        expect { subject.analyse(application, false) }.to output(result).to_stdout
+      end
+    end
+  end
+end
